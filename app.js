e3dfc471b9766029d1a2f211bac5ba040ca3d5ac
Filipe Crespo <filipe.crespo@locaweb.com.br>
2020-05-13 10:32:41 -0300

    Add port and host

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const socketIo = require('socket.io');
 
 const io = socketIo.listen(server);
 
-server.listen(3000,() => {
+const PORT = process.env.PORT || 3000;
+server.listen(PORT,() => {
   console.log("running")
 })
 
